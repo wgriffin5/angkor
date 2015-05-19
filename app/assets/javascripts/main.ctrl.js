923dc38640107359c8ea6337f186c7ea3f03b611
William Griffin <williamgriffin989@gmail.com>
2015-05-19 14:10:01 -0400

    initial commit

---
+++ app/assets/javascripts/main.ctrl.js
@@ -0,0 +1,130 @@
+angular.module('app').controller("MainController", function(){
+
+  var vm = this;
+
+  vm.title = 'Angkor';
+
+  vm.searchInput = '';
+
+  vm.companies = [
+
+    {
+        name: 'Gilead',
+        marketCap: 153,
+        holding: false
+
+    },
+
+    {
+        name: 'King Digital',
+        marketCap: 441,
+        holding: true
+
+    },
+
+    {
+        name: 'Wayside Technology',
+        marketCap: 84,
+        holding: false
+
+    },
+        
+    { 
+        name: 'GlobeImmune Inc',
+        marketCap: 55,
+        holding: false
+    },
+
+    { 
+        name: 'Gold Resource Corp',
+        marketCap: 189,
+        value: false
+
+    }
+
+  ];
+
+  vm.orders = [
+
+      {
+          id: 0,
+          name: 'Market Cap Ascending',
+          key: 'marketCap',
+          reverse: false
+
+      },
+
+      {
+          id: 1,
+          name: 'Market Cap Descending',
+          key: 'marketCap',
+          reverse: true
+      },
+
+      {
+
+          id: 2,
+          name: 'Name Ascending',
+          key: 'name',
+          reverse: false
+
+      },
+
+      {
+          id: 3,
+          name: 'Name Descending',
+          key: 'name',
+          reverse: true
+
+      }
+
+  ];
+
+  vm.order = vm.orders[1];
+
+  vm.new = {};
+  vm.addCompany = function() {
+
+      vm.companies.push(vm.new);
+      vm.new = {};
+
+  };
+
+  
+    
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
